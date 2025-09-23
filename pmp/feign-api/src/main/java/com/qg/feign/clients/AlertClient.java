ec6ddf9d3fb8248ac708efb149e6193efd77471b
tzdphxx <17811466341@163.com>
2025-09-23 13:02:51 +0800

    backend-service

--- pmp/feign-api/src/main/java/com/qg/feign/clients/AlertClient.java
+++ pmp/feign-api/src/main/java/com/qg/feign/clients/AlertClient.java
@@ -1,5 +1,8 @@
 package com.qg.feign.clients;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.qg.common.domain.po.Notification;
+import com.qg.common.domain.po.Responsibility;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
